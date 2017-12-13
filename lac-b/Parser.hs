7451203d18c1695fe1adf9de167aa237a0e53936
LiveNL <jordiwippert@live.nl>
2017-12-13 19:33:15 +0100

    Add all check functions

--- lac-b/Parser.hs
+++ lac-b/Parser.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS_GHC -w #-}
 module Parser where
+import Data.List
+import Debug.Trace
 import Control.Applicative(Applicative(..))
 import Control.Monad (ap)
 
@@ -467,7 +469,7 @@ data Pat = Lambda
          | Boundary
          | Empty
          | Rest
-  deriving Show
+  deriving (Show, Eq)
 
 data Dir = Right
          | Left
@@ -480,6 +482,7 @@ main = undefined
 type ProgramAlgebra p r x a = ([r] -> p,         -- program
                               Ident -> [x] -> r, -- rule
                               x,                 -- go
+                              x,                 -- take
                               x,                 -- mark
                               x,                 -- nothing
                               Dir -> x,          -- turn
@@ -488,10 +491,11 @@ type ProgramAlgebra p r x a = ([r] -> p,         -- program
                               Pat -> [x] -> a)   -- alt
 
 foldCmd :: ProgramAlgebra p r x a -> Program -> p
-foldCmd (program, rule, go, mark, nothing, turn, c, next, alt) = fp
+foldCmd (program, rule, go, take, mark, nothing, turn, c, next, alt) = fp
   where fp (Program xs) = program (map fr xs)
         fr (Rule x xs) = rule x (map ff xs)
         ff Go = go
+        ff Take = take
         ff Mark = mark
         ff Parser.Nothing = nothing
         ff (Turn x) = turn x
@@ -499,15 +503,35 @@ foldCmd (program, rule, go, mark, nothing, turn, c, next, alt) = fp
         ff (Next x) = next x
         fa (Alt x xs) = alt x (map ff xs)
 
--- hasStart :: ProgramAlgebra Bool Bool Bool Bool
--- hasStart = (all, (\x _ -> r x), (\x -> False), (\x -> False))
---  where r x = x == "start"
+-- There are no calls to undefined rules (rules may be used before they are defined though)
+notUndef :: ProgramAlgebra Bool (Ident,[Ident]) [Ident] [Ident]
+notUndef = ((\xs -> f xs), (\x xs -> (x, (concat xs))), [""], [""], [""], [""], (\x -> [""]), (\_ xs -> concat xs), (\x -> [x]), (\_ x -> concat x))
+  where f xs = all (flip elem (defs xs)) (snds xs)
+        defs xs = map fst xs
+        snds xs = filter (/="") (concat (map snd xs))
 
-countGo :: ProgramAlgebra Int Int Int Int
-countGo = (sum, (\_ -> sum), 1, 0, 0, (\_ -> 0), (\_ xs -> sum xs), (\_ -> 0), (\_ xs -> sum xs))
+-- No rule is defined twice
+notTwice :: ProgramAlgebra Bool Ident Bool Bool
+notTwice = ((\xs -> (length xs) == (length (nub xs))), (\x _ -> x), False, False, False, False, (\x -> False), (\_ _ -> False), (\x -> False), (\_ _ -> False))
 
--- check :: Program -> Bool
--- check = foldCmd hasStart
+-- There is a rule named start.
+hasStart :: ProgramAlgebra Bool Bool Bool Bool
+hasStart = ((\xs -> or xs), (\x _ -> r x), False, False, False, False, (\x -> False), (\_ _ -> False), (\x -> False), (\_ _ -> False))
+  where r x = x == "start"
+
+-- There is no possibility for pattern match failure, i.e., all case expressions must either contain a catch-all pattern _ or contain cases for all five other options.
+allPats :: ProgramAlgebra Bool Bool Bool Pat
+allPats = ((\xs -> or xs), (\_ xs -> or xs), False, False, False, False, (\x -> False), (\_ xs -> r xs), (\x -> False), (\x _ -> x))
+  where r xs = elem Rest xs || (length (nub xs) == 5) && (all def xs)
+        def x = elem x [Lambda, Debris, Asteroid, Boundary, Empty]
+
+-- check (Program (parseProgram [Token]))
+check :: Program -> Bool
+check p = and [u,t,s,a]
+  where u = foldCmd notUndef p
+        t = foldCmd notTwice p
+        s = foldCmd hasStart p
+        a = foldCmd allPats p
 {-# LINE 1 "templates/GenericTemplate.hs" #-}
 
 
