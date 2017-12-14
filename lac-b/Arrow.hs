972a50dc5214c458d2b1d256f950391f300c58a8
Kevin Wilbrink <kevin.wilbrink@Mgooglemail.com>
2017-12-14 11:09:28 +0100

    Exercise 9.

--- lac-b/Arrow.hs
+++ lac-b/Arrow.hs
@@ -2,12 +2,15 @@
 
 module Arrow where
 
-import Prelude hiding ((<*), (<$))
+import Prelude hiding ((<*), (<$), Right, Left, Nothing)
 import ParseLib.Abstract
 import Data.Map (Map)
 import qualified Data.Map as L
 import Control.Monad (replicateM)
 import Data.Char (isSpace)
+import qualified Data.Maybe as M
+
+import Debug.Trace
 
 
 type Space     =  Map Pos Contents
@@ -47,8 +50,83 @@ type Environment = Map Ident Commands
 
 type Stack       =  Commands
 data ArrowState  =  ArrowState Space Pos Heading Stack
+  deriving Show
 
 data Step  =  Done  Space Pos Heading
            |  Ok    ArrowState
            |  Fail  String
+  deriving Show
 
+newtype Program = Program { rules :: [Rule] }
+    deriving Show
+
+data Rule = Rule Ident [Cmd]
+    deriving Show
+
+data Cmd = Go
+         | Take
+         | Mark
+         | Nothing
+         | Turn Dir
+         | Case Dir [Alt]
+         | Next String
+    deriving (Eq, Show)
+
+data Alt = Alt Contents [Cmd]
+    deriving (Eq, Show)
+
+data Dir = Right
+         | Left
+         | Front
+    deriving (Eq, Show)
+
+{- Exercise 9 -}
+-- Test data
+spc = fst ( head ( parse parseSpace "(4,14)\n\\\\\\\\\\..........\n...............\n\\\\\\\\\\\\\\........\n...............\n...............\n"))
+env = L.fromList [("start",[Case Left [Alt Asteroid [Next "goOn"],Alt Boundary [Next "goOn"],Alt Lambda [Turn Left,Go,Take],Alt Rest [Turn Left,Go,Next "start"]]])]
+env1 = L.fromList [("start", [Turn Right,Go,Turn Left,Next "firstArg"])]
+arS = ArrowState spc (0,1) Right [Turn Right,Go,Turn Left,Next "firstArg"]
+
+step :: Environment -> ArrowState -> Step
+step e a@(ArrowState _ _ _ st) = action topItem a e
+  where topItem = head (st)
+
+action :: Cmd -> ArrowState -> Environment -> Step
+action Go (ArrowState sp p h (_:xs)) _ | field' == Empty || field' == Lambda || field' == Debris = Ok (ArrowState sp pos' h xs)
+                                       | otherwise                                               = Ok (ArrowState sp p    h xs) 
+  where field' = nextField h p sp
+        pos'   = nextPos h p
+
+action Take (ArrowState sp p h (_:xs)) _ = Ok (ArrowState sp' p h xs)
+  where sp' = L.insert p Empty sp
+
+action Mark (ArrowState sp p h (_:xs)) _ = Ok (ArrowState sp' p h xs)
+  where sp' = L.insert p Lambda sp
+
+action (Nothing) e _ = Ok e
+
+action (Turn x)   (ArrowState sp p _ (_:xs)) _ = Ok (ArrowState sp p x xs)
+
+action (Case x c) (ArrowState sp p h (_:xs)) _ | null test = Fail "Error"
+                                               | otherwise = Ok (ArrowState sp p h (test ++ xs))
+  where field' = nextField x p sp
+        test = getCmd field' c
+
+action (Next x) (ArrowState sp p h (_:xs)) e | find /= M.Nothing = Ok (ArrowState sp p h ((M.fromJust find) ++ xs))
+                                             | otherwise         = Fail "Stack is empty.."
+  where find = lookup x (L.toList e)
+
+getCmd :: Contents -> [Alt] -> [Cmd]
+getCmd f [] = []
+getCmd f ((Alt co cmd):xs) | co == f    = cmd
+                           | co == Rest = cmd
+                           | otherwise  = getCmd f xs
+
+nextField :: Heading -> Pos -> Space -> Contents
+nextField Right (x,y) s = M.fromJust (lookup (x, y + 1) (L.toList s))
+nextField Left  (x,y) s = M.fromJust (lookup (x, y - 1) (L.toList s))
+nextField _     _     _ = Boundary
+
+nextPos :: Heading -> Pos -> Pos
+nextPos Right (x, y) = (x, y + 1)
+nextPos Left  (x, y) = (x, 1 - y)
