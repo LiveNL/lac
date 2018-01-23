f73eda4418f0bc5af3f5c542cf6ea41f72673176
LiveNL <jordiwippert@live.nl>
2018-01-23 22:42:52 +0100

    Sort of working exercise 7

--- lac-c/src/CSharpCode.hs
+++ lac-c/src/CSharpCode.hs
@@ -8,13 +8,24 @@ import CSharpAlgebra
 import SSM
 
 import Data.Char
+import Debug.Trace
 
 data ValueOrAddress = Value | Address
     deriving Show
 
 type Env = M.Map String Int
+type DCode = (Code, [Decl], Env)
 
-codeAlgebra :: CSharpAlgebra Code Code (Env -> Code) (ValueOrAddress -> Env -> Code)
+frst :: (a, b, c) -> a
+frst (a,_,_) = a
+
+scnd :: (a, b, c) -> b
+scnd (_,b,_) = b
+
+thrd :: (a, b, c) -> c
+thrd (_,_,c) = c
+
+codeAlgebra :: CSharpAlgebra Code Code (Env -> DCode) (ValueOrAddress -> Env -> Code)
 codeAlgebra =
     ( fClas
     , (fMembDecl, fMembMeth)
@@ -28,8 +39,10 @@ fClas c ms = [Bsr "main", HALT] ++ concat ms
 fMembDecl :: Decl -> Code
 fMembDecl d = []
 
-fMembMeth :: Type -> Token -> [Decl] -> (Env -> Code) -> Code
-fMembMeth t (LowerId x) ps s = [LABEL x] ++ [LINK $ length ps] ++ s (toEnv M.empty ps) ++ [UNLINK] ++ [RET]
+fMembMeth :: Type -> Token -> [Decl] -> (Env -> DCode) -> Code
+fMembMeth t (LowerId x) ps s = [LABEL x] ++ [LINK l] ++ frst c ++ [UNLINK] ++ [RET]
+  where c = (s (toEnv M.empty ps))
+        l = (length (scnd c))
 
 toEnv :: Env -> [Decl] -> Env
 toEnv env ps = fst $ Prelude.foldr insertArgs (env, - 2) ps
@@ -40,29 +53,42 @@ insertArgs d (env, i) = (M.insert (key d) i env, i - 1)
 key :: Decl -> String
 key (Decl _ (LowerId x)) = x
 
-fStatDecl :: Decl -> Env -> Code
-fStatDecl d env = []
+dkey :: String -> Decl
+dkey x = (Decl (TypePrim (StdType "int")) (LowerId x))
 
-fStatExpr :: (ValueOrAddress -> Env -> Code) -> Env -> Code
-fStatExpr e env = e Value env ++ [pop]
+fStatDecl :: Decl -> Env -> DCode
+fStatDecl d env = ([], [d], newEnv)
+  where newEnv = M.insert (key d) (i + 1) env
+        i = M.size (M.filter (>= 0) env)
 
-fStatIf :: (ValueOrAddress -> Env -> Code) -> (Env -> Code) -> (Env -> Code) -> Env -> Code
-fStatIf e s1 s2 env = c ++ [BRF (n1 + 2)] ++ (s1 env) ++ [BRA n2] ++ (s2 env)
+fStatExpr :: (ValueOrAddress -> Env -> Code) -> Env -> DCode
+fStatExpr e env = (e Value env ++ [pop], [], env)
+
+fStatIf :: (ValueOrAddress -> Env -> Code) -> (Env -> DCode) -> (Env -> DCode) -> Env -> DCode
+fStatIf e s1 s2 env = (c ++ [BRF (n1 + 2)] ++ frst (s1 env) ++ [BRA n2] ++ frst (s2 env), [], env)
     where
         c        = e Value env
-        (n1, n2) = (codeSize (s1 env), codeSize (s2 env))
+        (n1, n2) = (codeSize (frst (s1 env)), codeSize (frst (s2 env)))
 
-fStatWhile :: (ValueOrAddress -> Env -> Code) -> (Env -> Code) -> Env -> Code
-fStatWhile e s1 env = [BRA n] ++ (s1 env) ++ c ++ [BRT (-(n + k + 2))]
+fStatWhile :: (ValueOrAddress -> Env -> Code) -> (Env -> DCode) -> Env -> DCode
+fStatWhile e s1 env = ([BRA n] ++ frst (s1 env) ++ c ++ [BRT (-(n + k + 2))], [], env)
     where
       c = e Value env
-      (n, k) = (codeSize (s1 env), codeSize c)
+      (n, k) = (codeSize (frst (s1 env)), codeSize c)
 
-fStatReturn :: (ValueOrAddress -> Env -> Code) -> Env -> Code
-fStatReturn e env = e Value env ++ [STR r3] ++ [pop] ++ [RET]
+fStatReturn :: (ValueOrAddress -> Env -> Code) -> Env -> DCode
+fStatReturn e env = (e Value env ++ [LDS 0, STR r3, pop, UNLINK, RET], [], env)
 
-fStatBlock :: [Env -> Code] -> Env -> Code
-fStatBlock x env = concatMap ($ env) x
+fStatBlock :: [Env -> DCode] -> Env -> DCode
+fStatBlock xs env = ((frst newDCode), (concatMap decls xs), (thrd newDCode))
+  where newDCode = if length xs > 1 then Prelude.foldl foldBlock ((head xs) env) xs         -- TODO: fix lame length check
+                                    else ((concatMap code xs), (concatMap decls xs), env)
+        decls dcode = scnd (dcode env)
+        code  dcode = frst (dcode env)
+
+foldBlock :: DCode -> (Env -> DCode) -> DCode
+foldBlock (code, decls, env) e = let (code', decls', env') = e env
+                                 in (code ++ code', decls ++ decls', env')
 
 fExprCon :: Token -> ValueOrAddress -> Env -> Code
 fExprCon (ConstInt n)  va env = [LDC n]
@@ -70,14 +96,15 @@ fExprCon (ConstChar n) va env = [LDC (digitToInt n)]
 fExprCon (ConstBool n) va env = [LDC (x n)]
   where x n = if n then 1 else 0
 
-fExprVar :: Token -> (ValueOrAddress -> Env -> Code)
-fExprVar (LowerId x) va env = let loc = env M.! x in case va of
-                                              Value    ->  [LDL  loc]
-                                              Address  ->  [LDLA loc]
+fExprVar :: Token -> ValueOrAddress -> Env -> Code
+fExprVar (LowerId x) va env = if member x env then let loc = env M.! x in case va of
+                                                        Value    ->  [LDL loc]
+                                                        Address  ->  [LDLA loc]
+                                              else error ("Missing var: " ++ x ++ ".")
 
 fExprOp :: Token -> (ValueOrAddress -> Env -> Code) -> (ValueOrAddress -> Env -> Code) -> ValueOrAddress -> Env -> Code
 fExprOp (Operator "=") e1 e2 va env = e2 Value env ++ [LDS 0] ++ e1 Address env ++ [STA 0]
-fExprOp (Operator op)  e1 e2 va env = e1 Value env ++ e2 Value env ++ [opCodes ! op]
+fExprOp (Operator op) e1 e2 va env = e1 Value env ++ e2 Value env ++ [opCodes ! op]
 
 fExprArg :: Token -> [ValueOrAddress -> Env -> Code] -> ValueOrAddress -> Env -> Code
 fExprArg (LowerId "print") es va env = concat [e Value env | e <- es] ++ [TRAP 0]
