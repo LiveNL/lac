a7888a45a6aded1612773bb7388b07d4b5d45359
LiveNL <jordiwippert@live.nl>
2018-01-25 12:58:49 +0100

    Indenting everywhere

--- lac-c/test-cases/10-test-fac-rec.cs
+++ lac-c/test-cases/10-test-fac-rec.cs
@@ -1,18 +1,12 @@
-class Hello
-{
-	void main()
-	{
+class Hello {
+	void main() {
 		print(fac(10));
 	}
-	
-	int fac(int x)
-	{
-		if (x == 1)
-		{
+
+	int fac(int x) {
+		if (x == 1) {
 			return 1;
-		}
-		else
-		{
+		} else {
 			return x * fac(x - 1);
 		}
 	}
