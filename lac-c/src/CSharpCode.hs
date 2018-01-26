cf36802893a9873af7ac6217acfb9a79c2a959fe
LiveNL <jordiwippert@live.nl>
2018-01-26 18:27:57 +0100

    Improve everthing

--- lac-c/src/CSharpCode.hs
+++ lac-c/src/CSharpCode.hs
@@ -6,6 +6,7 @@ import CSharpLex
 import CSharpGram
 import CSharpAlgebra
 import SSM
+import Data.List
 
 import Data.Char
 import Debug.Trace
@@ -16,14 +17,15 @@ data ValueOrAddress = Value | Address
 type Env = M.Map String Int
 type DCode = (Code, [Decl], Env)
 
-frst :: (a, b, c) -> a
-frst (a,_,_) = a
+-- Code Decls Env, methods to achieve the first, second and third part of a triple.
+frst :: (c, d, e) -> c
+frst (c, _, _) = c
 
-scnd :: (a, b, c) -> b
-scnd (_,b,_) = b
+scnd :: (c, d, e) -> d
+scnd (_, d, _) = d
 
-thrd :: (a, b, c) -> c
-thrd (_,_,c) = c
+thrd :: (c, d, e) -> e
+thrd (_, _, e) = e
 
 codeAlgebra :: CSharpAlgebra Code Code (Env -> DCode) (ValueOrAddress -> Env -> Code)
 codeAlgebra =
@@ -53,12 +55,10 @@ insertArgs d (env, i) = (M.insert (key d) i env, i - 1)
 key :: Decl -> String
 key (Decl _ (LowerId x)) = x
 
-dkey :: String -> Decl
-dkey x = (Decl (TypePrim (StdType "int")) (LowerId x))
-
 fStatDecl :: Decl -> Env -> DCode
 fStatDecl d env = ([], [d], newEnv)
-  where newEnv = M.insert (key d) (i + 1) env
+  where newEnv = if member (key d) env then env
+                 else M.insert (key d) (i + 1) env
         i = M.size (M.filter (>= 0) env)
 
 fStatExpr :: (ValueOrAddress -> Env -> Code) -> Env -> DCode
@@ -66,25 +66,25 @@ fStatExpr e env = (e Value env ++ [pop], [], env)
 
 fStatIf :: (ValueOrAddress -> Env -> Code) -> (Env -> DCode) -> (Env -> DCode) -> Env -> DCode
 fStatIf e s1 s2 env = (c ++ [BRF (n1 + 2)] ++ frst (s1 env) ++ [BRA n2] ++ frst (s2 env), [], env)
-    where
-        c        = e Value env
+  where c        = e Value env
         (n1, n2) = (codeSize (frst (s1 env)), codeSize (frst (s2 env)))
 
 fStatWhile :: (ValueOrAddress -> Env -> Code) -> (Env -> DCode) -> Env -> DCode
 fStatWhile e s1 env = ([BRA n] ++ frst (s1 env) ++ c ++ [BRT (-(n + k + 2))], [], env)
-    where
-      c = e Value env
-      (n, k) = (codeSize (frst (s1 env)), codeSize c)
+  where c = e Value env
+        (n, k) = (codeSize (frst (s1 env)), codeSize c)
 
 fStatReturn :: (ValueOrAddress -> Env -> Code) -> Env -> DCode
-fStatReturn e env = (e Value env ++ [LDS 0, STR r3, pop, UNLINK, RET], [], env)
+fStatReturn e env = (e Value env ++ [STR r3], [], env)
 
 fStatBlock :: [Env -> DCode] -> Env -> DCode
-fStatBlock xs env = ((frst newDCode), (concatMap decls xs), (thrd newDCode))
-  where newDCode = if length xs > 1 then Prelude.foldl foldBlock ((head xs) env) xs         -- TODO: fix lame length check
-                                    else ((concatMap code xs), (concatMap decls xs), env)
+fStatBlock xs env = ((frst newDCode), (scnd newDCode), (thrd newDCode))
+  where newDCode = if Data.List.null decls' then (code', decls', env)
+                                            else Prelude.foldl foldBlock ((head xs) env) xs
+        code dcode = frst (dcode env)
+        code' = concatMap code xs
         decls dcode = scnd (dcode env)
-        code  dcode = frst (dcode env)
+        decls' = concatMap decls xs
 
 foldBlock :: DCode -> (Env -> DCode) -> DCode
 foldBlock (code, decls, env) e = let (code', decls', env') = e env
@@ -92,7 +92,7 @@ foldBlock (code, decls, env) e = let (code', decls', env') = e env
 
 fExprCon :: Token -> ValueOrAddress -> Env -> Code
 fExprCon (ConstInt n)  va env = [LDC n]
-fExprCon (ConstChar n) va env = [LDC (digitToInt n)]
+fExprCon (ConstChar n) va env = [LDC (ord n)]
 fExprCon (ConstBool n) va env = [LDC (x n)]
   where x n = if n then 1 else 0
 
@@ -109,7 +109,7 @@ fExprOp (Operator op) e1 e2 va env = e1 Value env ++ e2 Value env ++ [opCodes ! 
 
 fExprArg :: Token -> [ValueOrAddress -> Env -> Code] -> ValueOrAddress -> Env -> Code
 fExprArg (LowerId "print") es va env = concat [e Value env | e <- es] ++ [TRAP 0]
-fExprArg (LowerId x)       es va env = concat [e Value env | e <- es] ++ [Bsr x] ++ [LDR r3]
+fExprArg (LowerId x)       es va env = concat [e Value env | e <- es] ++ [Bsr x] ++ [pop] ++ [LDR r3]
 
 fExprOpU :: Token -> (ValueOrAddress -> Env -> Code) -> ValueOrAddress -> Env -> Code
 fExprOpU (Operator "++") e1 va env = e1 Value env ++ [LDC 1, ADD, LDS 0] ++ e1 Address env ++ [STA 0]
